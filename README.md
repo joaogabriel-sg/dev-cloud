b6ef4768ae58bd8c991fd65d86b2aacfda630421
João Gabriel <joaogabrielsg.00@gmail.com>
2021-02-01 10:19:05 -0300

    :building_construction: Basis of the project

---
+++ README.md
@@ -0,0 +1,33 @@
+<div align="center">
+ <img src="./public/assets/logo.svg">
+</div>
+
+**Dev Cloud** is a personal study site for a fictional cloud platform, which I was able to do from design to coding.  
+See live on [Dev Cloud 🔥](https://joaogabriel-sg.github.io/dev-cloud/)
+
+## 📚 What did I learn from the project??
+
+
+## 🚀 Features
+
+- HTML
+- CSS
+- Sass
+- JavaScript
+- ESLint
+- Gulp
+- Babel
+
+## 🎮 Getting started
+
+1. Clone this repository using `git clone https://github.com/joaogabriel-sg/dev-cloud.git`
+
+1. Move yourself to the appropriate repository `cd dev-cloud`
+
+1. Run `npm i` to install the packages dependencies
+
+1. Run `gulp` on terminal to run the application
+
+## 📃 License
+
+This project is licensed under the MIT License - see the [LICENSE](https://choosealicense.com/licenses/mit/) page for details.
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+sass.compiler = require('node-sass');
+
+function compileSass() {
+  return gulp
+    .src('./public/css/scss/style.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream());
+}
+
+function runBuild() {
+  return gulp
+    .src([
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      './public/js/**/*.js',
+    ])
+    .pipe(babel({
+      presets: ['@babel/preset-env'],
+    }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+  gulp.watch('./public/css/scss/**/*.scss', compileSass);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./public/js/**/*.js', runBuild);
+}
+
+gulp.task('default', watch);
